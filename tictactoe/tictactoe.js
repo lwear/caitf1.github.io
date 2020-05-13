e422f29f4d0fa52926dccb4ff89808fc9287c38e
caitf1 <64191629+caitf1@users.noreply.github.com>
2020-05-13 14:28:21 -0700

    Final upload

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,21 +1,10 @@
 let currentPlayer = "X";
-let cb = []; // current board
+let cb = [];
 let gameStatus = ""; // "" - continue game, "Tie", "X Wins", "O Wins"
 let numTurns = 0;
 
 let idNames = ["one", "two", "three", "four", "five", 
 				"six", "seven", "eight", "nine"];
-
-const winningCombos = [
-["one", "two", "three"],
-["four", "five", "six"],
-["seven", "eight", "nine"],
-["one", "four", "seven"],
-["two", "five", "eight"],
-["three", "six", "nine"],
-["one", "five", "nine"],
-["three", "five", "seven"]
-];
 
 //reset board and all variables
 function newGame() {
@@ -37,58 +26,151 @@ function newGame() {
 function computerTakeTurn() {
 	let idName = "";
 	
-	// choose random boxes until an empty box is found
-	do {
-		
-		if((document.getElementById(("one"&&"two")||("two"&&"three")||("one"&"three")).innerHTML = "X")||(document.getElementById(("one"&&"two")||("two"&&"three")||("one"&"three")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*3) + 1; //1-3
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("four"&&"five")||("four"&&"six")||("five"&"six")).innerHTML = "X")||(document.getElementById(("four"&&"five")||("four"&&"six")||("five"&"six")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*6) + 4; //3-6
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("seven"&&"eight")||("eight"&&"nine")||("seven"&"nine")).innerHTML = "X")||(document.getElementById(("seven"&&"eight")||("eight"&&"nine")||("seven"&"nine")){
-		let rand = parseInt(Math.random()*9) + 7; //6-9
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("one"&&"four")||("four"&&"seven")||("one"&"seven")).innerHTML = "X")||(document.getElementById(("one"&&"four")||("four"&&"seven")||("one"&"seven")).innerHTML = "X")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*(1||4||7)); //1,4,7
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("five"&&"two")||("two"&&"eight")||("five"&"eight")).innerHTML = "X")||(document.getElementById(("five"&&"two")||("two"&&"eight")||("five"&"eight")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*(2||5||8); //2,5,8
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("three"&&"six")||("six"&&"nine")||("nine"&"three")).innerHTML = "X")||(document.getElementById(("three"&&"six")||("six"&&"nine")||("nine"&"three")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*(3||6||9)); //3,6,9
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("one"&&"five")||("five"&&"nine")||("one"&"nine")).innerHTML = "X")||(document.getElementById(("one"&&"five")||("five"&&"nine")||("one"&"nine")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*(1||5||9)); //1,5,9
-		idName = idNames[rand-1];
-		}
-		
-		if((document.getElementById(("three"&&"five")||("seven"&&"three")||("five"&"seven")).innerHTML = "X")||(document.getElementById(("three"&&"five")||("seven"&&"three")||("five"&"seven")).innerHTML = "O")){
-		let rand = parseInt(Math.random()*(3||5||7); //3,5,7
-		idName = idNames[rand-1];
-		}
-		
-		//check if chosen box is empty
-		if(document.getElementById(idName).innerHTML = "") {
-		document.getElementById(idName).innerHTML = currentPlayer;
-		break;
-		}
-		
-	} while(true);
+	cb[0] = ""; // not going to use
+	cb[1] = document.getElementById("one").innerHTML;
+	cb[2] = document.getElementById("two").innerHTML;
+	cb[3] = document.getElementById("three").innerHTML;
+	cb[4] = document.getElementById("four").innerHTML;
+	cb[5] = document.getElementById("five").innerHTML;
+	cb[6] = document.getElementById("six").innerHTML;
+	cb[7] = document.getElementById("seven").innerHTML;
+	cb[8] = document.getElementById("eight").innerHTML;
+	cb[9] = document.getElementById("nine").innerHTML;
 	
+	// choose random boxes until an empty box is found
+		
+	//check first row
+	if ( cb[1] == cb[2] && cb[3] == "") {
+	   idName = idNames[2];  
+	}else if (cb[1] == cb[3] && cb[2] == ""){
+	   idName = idNames[1];  
+	} else if  (cb[2] == cb[3] && cb[1] == "") {
+	  idName = idNames[0];
+	} else {
+	   do {
+		  idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+		
+	
+	//check second row
+	if ( cb[4] == cb[5] && cb[6] == "") {
+	   idName = idNames[5];  
+	}else if (cb[4] == cb[6] && cb[5] == ""){
+	   idName = idNames[4];  
+	} else if  (cb[5] == cb[6] && cb[14] == "") {
+	  idName = idNames[3];
+	} else {
+	   do {
+		  idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+		
+	
+	//check third row
+	if ( cb[7] == cb[8] && cb[9] == "") {
+	   idName = idNames[8];  
+	}else if (cb[7] == cb[9] && cb[8] == ""){
+	   idName = idNames[7];  
+	} else if  (cb[8] == cb[9] && cb[7] == "") {
+	  idName = idNames[6];
+	} else {
+	   do {
+		  idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+		
+	//check first column
+	if ( cb[1] == cb[4] && cb[7] == "") {
+	   idName = idNames[6];  
+	}else if (cb[1] == cb[7] && cb[4] == ""){
+	   idName = idNames[3];  
+	} else if  (cb[4] == cb[7] && cb[1] == "") {
+	  idName = idNames[0];
+	} else {
+	   do {
+		  idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+		
+	//check second column
+	if ( cb[2] == cb[5] && cb[8] == "") {
+	   idName = idNames[7];  
+	}else if (cb[2] == cb[8] && cb[5] == ""){
+	   idName = idNames[4];  
+	} else if  (cb[5] == cb[8] && cb[2] == "") {
+	  idName = idNames[1];
+	} else {
+	   do {
+		  idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+		
+	//check third column
+	if ( cb[3] == cb[6] && cb[9] == "") {
+	   idName = idNames[8];  
+	}else if (cb[3] == cb[9] && cb[6] == ""){
+	   idName = idNames[5];  
+	} else if  (cb[6] == cb[9] && cb[3] == "") {
+	  idName = idNames[2];
+	} else {
+	   do {
+		   idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+		
+	//check L-R diagonal
+	if ( cb[1] == cb[5] && cb[9] == "") {
+	   idName = idNames[8];  
+	}else if (cb[1] == cb[9] && cb[5] == ""){
+	   idName = idNames[4];  
+	} else if  (cb[5] == cb[9] && cb[1] == "") {
+	  idName = idNames[0];
+	} else {
+	   do {
+		  idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
 
+	//check R-L diagonal
+	if ( cb[3] == cb[5] && cb[7] == "") {
+	   idName = idNames[6];  
+	}else if (cb[7] == cb[3] && cb[5] == ""){
+	   idName = idNames[4];  
+	} else if  (cb[5] == cb[7] && cb[3] == "") {
+	  idName = idNames[2];
+	} else {
+	   do {
+		   idName = idNames[parseInt(Math.random()*9)];
+		  if(document.getElementById(idName).innerHTML == "") {  
+				break;
+		  }
+		}while(true);
+	} // else
+	
+	document.getElementById(idName).innerHTML = currentPlayer;
+		
 } //computerTakeTurn
 
 // take player turn 
@@ -121,11 +203,9 @@ function checkGameStatus() {
 	// check for a win
 	if (checkWin()) {
 		gameStatus = currentPlayer + " wins!";
-		return;
-	}
+	}//check for tie
 	
-	//check for tie
-	if (numTurns ==9){
+	else if (numTurns ==9){
 		gameStatus = "Tie Game!";
 	}
 	
